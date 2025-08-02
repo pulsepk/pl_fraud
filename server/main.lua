f80fc70c51349714204ac86756e46613971256da
Hassan <75420576+pulsepk@users.noreply.github.com>
2025-08-03 03:46:58 +0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -184,7 +184,7 @@ local WaterMark = function()
     SetTimeout(1500, function()
         print('^1['..resourceName..'] ^2Thank you for Downloading the Script^0')
         print('^1['..resourceName..'] ^2If you encounter any issues please Join the discord https://discord.gg/c6gXmtEf3H to get support..^0')
-        print('^1['..resourceName..'] ^2Enjoy a secret 20% OFF any script of your choice on https://pulsescripts.tebex.io/freescript^0')
+        print('^1['..resourceName..'] ^2Enjoy a secret 20% OFF any script of your choice on https://pulsescripts.com/^0')
         print('^1['..resourceName..'] ^2Using the coupon code: SPECIAL20 (one-time use coupon, choose wisely)^0')
     
     end)
