644856e5a4bcabe0d56395eb95562a3811f93b2d
pulsepk <hassannazar72@gmail.com>
2026-06-04 21:39:14 +0100

    Hook shop ped target into pl_lib instead of manual per-system checks

--- client/shop.lua
+++ client/shop.lua
@@ -2,12 +2,9 @@ if not Config.Shop.Enable then return end
 
 local shopped
 
-local targetResource = GetTarget()
-
 print(("^2[Shop] Initializing shop: ^7%s"):format(Config.Shop.name))
 
 CreateThread(function()
-    -- Spawn Ped
     RequestModel(Config.Shop.pedModel)
     while not HasModelLoaded(Config.Shop.pedModel) do Wait(0) end
 
@@ -16,7 +13,6 @@ CreateThread(function()
     FreezeEntityPosition(shopped, true)
     SetBlockingOfNonTemporaryEvents(shopped, true)
 
-    -- Add Blip
     if Config.Shop.blip.enabled then
         local blip = AddBlipForCoord(Config.Shop.coords.x, Config.Shop.coords.y, Config.Shop.coords.z)
         SetBlipSprite(blip, Config.Shop.blip.sprite)
@@ -29,44 +25,23 @@ CreateThread(function()
         EndTextCommandSetBlipName(blip)
     end
 
-    -- Add Target Option
-    local openShopAction = function()
-        if GetResourceState('ox_inventory') == 'started' then
-            exports.ox_inventory:openInventory('shop', { type = Config.Shop.id, id = 1 })
-        elseif GetResourceState('qb-inventory') == 'started' then
-            TriggerServerEvent('pl_fraud:server:OpenShopQB')
-        end
-    end
+    exports.pl_lib:AddEntityTarget(shopped, {
+        name = 'fraud_open_shop',
+        label = 'Open Shop',
+        icon = 'fa-solid fa-shop',
+        distance = 2.0,
+        event = 'pl_fraud:client:openShop',
+    })
+end)
 
-    if targetResource == 'ox_target' then
-        exports.ox_target:addLocalEntity(shopped, {
-            {
-                label = "Open Shop",
-                icon = "fa-solid fa-shop",
-                onSelect = openShopAction
-            }
-        })
-
-    elseif targetResource == 'qb-target' then
-        exports['qb-target']:AddTargetEntity(shopped, {
-            options = {
-                {
-                    type = "client",
-                    event = "custom:openShop",
-                    icon = "fa-solid fa-shop",
-                    label = "Open Shop"
-                }
-            },
-            distance = 2.0
-        })
-
-        RegisterNetEvent("custom:openShop", function()
-            openShopAction()
-        end)
+AddEventHandler('pl_fraud:client:openShop', function()
+    if GetResourceState('ox_inventory') == 'started' then
+        exports.ox_inventory:openInventory('shop', { type = Config.Shop.id, id = 1 })
+    elseif GetResourceState('qb-inventory') == 'started' then
+        TriggerServerEvent('pl_fraud:server:OpenShopQB')
     end
 end)
 
--- Clean up on stop
 AddEventHandler('onResourceStop', function(resourceName)
     if GetCurrentResourceName() ~= resourceName then return end
     if DoesEntityExist(shopped) then
